601be2ca4e0b7c3357d722b9a00750bae4b04606
kooodaisportfolio <47857309+kooodaisportfolio@users.noreply.github.com>
2019-02-22 16:52:11 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  $('.box-one').hover(function(){
+   $('#sentence').fadeIn();
+  },function(){
+    $('#sentence').fadeOut();
+  });
+
+  $('.box-two').hover(function(){
+   $('#mail').fadeIn();
+  },function(){
+    $('#mail').fadeOut();
+  });
+
+
+});
